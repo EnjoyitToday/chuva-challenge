e93088ef1e0774f5dfe5b1c6942ec42a9ba49320
CAUE <caue.ms01@gmail.com>
2023-10-09 11:39:30 -0300

    first commit

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarModule } from './sidebar/sidebar.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SidebarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
